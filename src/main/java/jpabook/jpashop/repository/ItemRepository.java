bdcb7df0b9ca8c8dd635ecb6f7d413cf0ef66bc7
HanYoonSoo <hdbstn3055@gmail.com>
2024-01-22 16:25:03 +0900

    feat: Item Domain

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    public Item findItemById(Long id);
+
+    @Query("select i from Item i")
+    public List<Item> findAll();
+
+}
